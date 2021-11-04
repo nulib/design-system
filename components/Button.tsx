795ff9f756f0167bf368d6765a697d4f639e01df
Adam J. Arling <aarling@gmail.com>
2021-11-04 14:57:17 -0500

    Initial structure in place pulling in Stiches

---
+++ components/Button.tsx
@@ -0,0 +1,30 @@
+import { styled } from "../stitches.config";
+
+export const Button = styled("button", {
+  // Reset
+  all: "unset",
+  alignItems: "center",
+  boxSizing: "border-box",
+  userSelect: "none",
+  "&::before": {
+    boxSizing: "border-box",
+  },
+  "&::after": {
+    boxSizing: "border-box",
+  },
+
+  // Custom reset?
+  display: "inline-flex",
+  flexShrink: 0,
+  justifyContent: "center",
+  lineHeight: "1",
+  WebkitTapHighlightColor: "rgba(0,0,0,0)",
+
+  // Custom
+  height: "$5",
+  px: "$2",
+  fontFamily: "$untitled",
+  fontSize: "$2",
+  fontWeight: 500,
+  backgroundColor: "$blue10",
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Head>
+        <title>Northwestern Libraries Design System</title>
+        <meta
+          name="description"
+          content="Northwestern University Libraries Design System"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default App;
