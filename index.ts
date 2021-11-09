12241b2b609d2cfd0d5ae08d667316c89aa51366
Adam J. Arling <aarling@gmail.com>
2021-11-09 09:30:11 -0600

    UInclude Tag, Section and SpacerLine components in package export

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 export { Button } from "./components/Button";
 export { Notification } from "./components/Notification";
+export { Section } from "./components/Section";
+export { SpacerLine } from "./components/SpacerLine";
+export { Tag } from "./components/Tag";
 
 // Stitches
 export {
