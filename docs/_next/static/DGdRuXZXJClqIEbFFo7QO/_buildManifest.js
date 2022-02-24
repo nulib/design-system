23b589be546242c32718d1e0ecb92b99865f38c6
Adam J. Arling <aarling@gmail.com>
2022-02-24 09:16:52 -0800

    Update build script and delete /docs Github pages folder no longer needed

--- docs/_next/static/DGdRuXZXJClqIEbFFo7QO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/76-80e9741564067e80.js","static/chunks/pages/index-0716df8d3a95ab84.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
