06caebe79dfac528503e5f5735fc4d6c46ae945d
adamjarling <adamjarling@users.noreply.github.com>
2022-02-24 18:24:08 +0000

    Deploying to gh-pages from @ nulib/design-system@2cb417eb6215fd5e0632c2ade56c96bf932f824b 🚀

---
+++ _next/static/FwAYRGxrCUq2BFQXYNEqI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/76-24c61de507141148.js","static/chunks/pages/index-ce71eea8e0f96512.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
